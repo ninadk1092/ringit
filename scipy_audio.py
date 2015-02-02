7a36df067b750d60f6a97a5e1429bd003845b887
Ninad Kulkarni <invertclock@gmail.com>
2015-02-02 17:27:32 +0530

    fft commit

--- scipy_audio.py
+++ scipy_audio.py
@@ -1,10 +1,38 @@
+import numpy as np
+import scipy
 from scipy.io.wavfile import read
+from scipy.signal import hann
+from scipy.fftpack import rfft
 import matplotlib.pyplot as plt
 
+#read audio samples
 input = read("01. The Eagles - Hotel California.wav")
 audio = input[1]
+
+#apply Hanning window
+window = hann(1024)
+audio = audio[0:1024]
+
+#fft
+mags = abs(rfft(audio))
+
+#convert to dB
+mags = 20*scipy.log10(mags)
+
+#normalise to 0 dB max
+#mags -= max(mags)
+
+#plot 
+plt.plot(mags)
+plt.ylabel("Magnitude (dB)")
+plt.xlabel("Frequency")
+plt.title("Hotel California Fourier Domain")
+plt.show()
+
+'''
 plt.plot(audio[0:10000000000])
 plt.ylabel("Amplitude")
 plt.xlabel("Time(samples)")
 plt.title("Kid in Town")
 plt.show()
+'''
