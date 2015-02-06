fbbcf0e69398a430b948f54acaffa77d00206356
Ninad Kulkarni <invertclock@gmail.com>
2015-02-06 14:56:16 +0530

    auto commit

--- time_beats.py
+++ time_beats.py
@@ -16,14 +16,18 @@ audio = input[1]
 Bavg_list = []
 BmaxSavg_list = []
 
-for BW in xrange(0,len(audio), 50):
-  Bwin = audio[BW:BW+100]
+#BW increments by 85ms each iteration.
+#SW increments 5ms each iteration
+#44.1 samples per ms. So, 220.5 ~ 221 samples per 5ms. And 3748.5 ~ 3749 increments every 85ms.
+
+for BW in xrange(0,len(audio),3749):
+  Bwin = audio[BW:BW+4410]
   Bavg = sum(abs(Bwin))/len(Bwin)
   Bavg_list.append(Bavg)
   Savg_list = []
 
-  for SW in xrange(BW, BW+100, 10):
-    Swin = audio[SW:SW+20]
+  for SW in xrange(BW, BW+4410, 221):
+    Swin = audio[SW:SW+882]
     if len(Swin)>0:
       Savg = sum(abs(Swin))/len(Swin)
     Savg_list.append(Savg)
@@ -31,9 +35,14 @@ for BW in xrange(0,len(audio), 50):
   #maxS = max(Savg_list)
   BmaxSavg_list.append(Savg_list)
 
-x = np.linspace(0, len(BmaxSavg_list), len(BmaxSavg_list) -1)   #np.linspace(start,stop,number_of_numbers) 
+print type(BmaxSavg_list)
+
+x=np.arange(len(BmaxSavg_list))
+print type(x)
+#x = np.linspace(0, len(BmaxSavg_list), len(BmaxSavg_list) +1)   #np.linspace(start,stop,number_of_numbers) 
 print len(x), len(BmaxSavg_list)
 plt.scatter(x,BmaxSavg_list)
 plt.autoscale(tight=True)
 plt.grid()
 plt.show()
+
