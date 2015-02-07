09f3890921b2ddbeaf736d5eb03271c613c521af
Ninad Kulkarni <invertclock@gmail.com>
2015-02-07 18:51:29 +0530

    auto commit

--- time_beats.py
+++ time_beats.py
@@ -9,6 +9,7 @@ sample_per_sec = input[0]     #input[0] contains samples per second. 44.1k in th
 print "-------------"
 
 audio = input[1]  
+audio = abs(audio)
 
 #len(audio) = 13230000. Duration of song = 300 sec. But actual duration is 390. Check why this is happening. 
 #44.1k samples per second is correct according to audacity. 
@@ -20,23 +21,28 @@ BmaxSavg_list = []
 #SW increments 5ms each iteration
 #44.1 samples per ms. So, 220.5 ~ 221 samples per 5ms. And 3748.5 ~ 3749 increments every 85ms.
 
+markers = []
+
 for BW in xrange(0,len(audio),3749):
   Bwin = audio[BW:BW+4410]
-  Bavg = sum(abs(Bwin))/len(Bwin)
-  Bavg_list.append(Bavg)
-  Savg_list = []
+  #max = sum(i for i in Bwin)/len(Bwin)  
+  max = np.mean(Bwin)
+
+  SWindex = BW
 
   for SW in xrange(BW, BW+4410, 221):
     Swin = audio[SW:SW+882]
-    if len(Swin)>0:
-      Savg = sum(abs(Swin))/len(Swin)
-    Savg_list.append(Savg)
+    #if (sum(abs(j) for j in Swin)/len(Swin)) > max:
+    if (np.mean(Swin)) > max:
+	SWindex = SW
+	max = np.mean(Swin)
+
+  markers.append(SWindex)
   
-  #maxS = max(Savg_list)
-  BmaxSavg_list.append(Savg_list)
+print markers
 
-print type(BmaxSavg_list)
 
+'''
 x=np.arange(len(BmaxSavg_list))
 print type(x)
 #x = np.linspace(0, len(BmaxSavg_list), len(BmaxSavg_list) +1)   #np.linspace(start,stop,number_of_numbers) 
@@ -45,4 +51,4 @@ plt.scatter(x,BmaxSavg_list)
 plt.autoscale(tight=True)
 plt.grid()
 plt.show()
-
+'''
