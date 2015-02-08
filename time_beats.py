3dee7b0ba75cf54c5e87e8cc876e7f958d44fa46
Ninad Kulkarni <invertclock@gmail.com>
2015-02-08 15:33:59 +0530

    auto commit

--- time_beats.py
+++ time_beats.py
@@ -22,33 +22,28 @@ BmaxSavg_list = []
 #44.1 samples per ms. So, 220.5 ~ 221 samples per 5ms. And 3748.5 ~ 3749 increments every 85ms.
 
 markers = []
-
+bigwindow = []
 for BW in xrange(0,len(audio),3749):
   Bwin = audio[BW:BW+4410]
   #max = sum(i for i in Bwin)/len(Bwin)  
   max = np.mean(Bwin)
-
+  bigwindow.append(BW)
   SWindex = BW
 
   for SW in xrange(BW, BW+4410, 221):
     Swin = audio[SW:SW+882]
-    #if (sum(abs(j) for j in Swin)/len(Swin)) > max:
     if (np.mean(Swin)) > max:
 	SWindex = SW
 	max = np.mean(Swin)
 
   markers.append(SWindex)
-  
-print markers
+ 
+print len(markers) 
+y = np.empty(len(markers))
+y.fill(3)
 
-
-'''
-x=np.arange(len(BmaxSavg_list))
-print type(x)
-#x = np.linspace(0, len(BmaxSavg_list), len(BmaxSavg_list) +1)   #np.linspace(start,stop,number_of_numbers) 
-print len(x), len(BmaxSavg_list)
-plt.scatter(x,BmaxSavg_list)
-plt.autoscale(tight=True)
-plt.grid()
+plt.plot(markers,y,'r|')
+plt.plot(bigwindow,y,'b|')
+plt.axis([0,132300,0,6])
 plt.show()
-'''
+
